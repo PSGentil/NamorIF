4803d6178579224e32df048d215b58a7867e900f
Monique <79199057+moniq-e@users.noreply.github.com>
2022-11-17 11:40:22 -0300

    progredindo...

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import cors from 'cors'
 const app = express()
 
 app.use(cors())
+app.use(express.static('public'))
 
-app.post('/', (req, res) => {
+app.put('/api', (req, res) => {
+    res.status(200)
+    res.send({msg: "oi"})
     res.end()
+    console.log(req.body)
 })
 
 app.listen(3000, () => {
