da698ee05d060db16c51bf1a8a900c3fd6b49336
Pato de Sapato <112494130+PatoDeSapatos@users.noreply.github.com>
2022-11-18 09:26:06 -0300

    starting log in functions

--- public/js/index.js
+++ public/js/index.js
@@ -1,35 +1,54 @@
-const options = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json; charset=UTF-8' },
-    body: JSON.stringify({
-        user: 'id',
-        pass: 'asdhoasjd'
-    })
-}
+// Index Buttons Functions
+import{ enviarLogin } from './login.js'
 
-fetch('http://localhost:3000/api/login', options)
 
-let menuActive = false
+let menuActive, logPageOn = false
 
-function expand(){
-    
+function expand() {
+
     let navBar = document.getElementsByTagName('nav')
     let icons = document.getElementsByClassName('icons')
 
-    if (!menuActive){
+    if (!menuActive) {
         navBar[0].style.width = '50%'
         icons[0].style.margin = '0 0 0 90%'
         icons[1].style.margin = '0 0 0 90%'
         menuActive = true
-    }else{
+    } else {
         navBar[0].style.width = '5%'
         icons[0].style.margin = '0 auto'
         icons[1].style.margin = '0 auto'
         menuActive = false
     }
-    
+
 }
 
-function loginPage(){
-    
+function logPage() {
+
+    let logPage = document.getElementById('logPage')
+
+    if (!logPageOn) {
+        logPage.style.display = 'block'
+        logPageOn = true
+    } else {
+        logPage.style.display = 'none'
+        logPageOn = false
+    }
+
+}
+
+function login() {
+
+    let email = document.getElementById('getEmail')
+    let pass =  document.getElementById('getPass')
+    let passConfirm =  document.getElementById('getPassConfirm')
+
+    if(pass == passConfirm){
+        enviarLogin(email, pass).then(res => {
+            
+        })
+    }else{
+        window.alert("MUDA")
+    }
+
 }
---
+++ public/js/login.js
@@ -0,0 +1,27 @@
+let email = window.localStorage.getItem('email')
+let password = window.localStorage.getItem('password')
+
+if (email) {
+
+    enviarLogin(email, password).then(async res => {
+        let body = await res.json()
+
+        if (body.isLogged) {
+
+        }
+
+    })
+
+}
+
+export function enviarLogin(email, pass) {
+    return fetch('http://localhost:3000/api/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json; charset=UTF-8' },
+        body: JSON.stringify({
+            email: email,
+            pass: pass
+        })
+    })
+}
+
