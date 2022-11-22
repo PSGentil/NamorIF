b32f6f1de4627629cdc723f79fe45e2f69ce0708
Pato de Sapato <112494130+PatoDeSapatos@users.noreply.github.com>
2022-11-22 09:25:13 -0300

    log in pop up improved

--- public/js/index.js
+++ public/js/index.js
@@ -3,15 +3,14 @@ import { enviarLogin } from './login.js'
 
 let menuActive, logPageOn
 
-const menu = document.getElementById('menu')
-menu.onclick = () => {
+document.getElementById('menu').addEventListener('click', e => {
     let navBar = document.getElementsByTagName('nav')
     let icons = document.getElementsByClassName('icons')
 
     if (!menuActive) {
-        navBar[0].style.width = '50%'
+        navBar[0].style.width = '30%'
         for (const icon of icons) {
-            icon.style.margin = '0 0 0 90%'
+            icon.style.margin = '0 0 0 85%'
         }
         menuActive = true
     } else {
@@ -21,23 +20,29 @@ menu.onclick = () => {
         }
         menuActive = false
     }
-}
+})
 
-const loginIcon = document.getElementById('loginIcon')
-loginIcon.onclick = () => {
-    let logPage = document.getElementById('logPage')
+function logPageSwitch(e){
+    let isLogged = window.localStorage.getItem('isLogged')
 
-    if (!logPageOn) {
-        logPage.style.display = 'block'
-        logPageOn = true
+    if (!isLogged) {
+        let logPage = document.getElementById('logPage')
+
+        if (!logPageOn) {
+            logPage.style.display = 'block'
+            logPageOn = true
+        } else {
+            logPage.style.display = 'none'
+            logPageOn = false
+        }
     } else {
-        logPage.style.display = 'none'
-        logPageOn = false
+        window.location.href = '../pages/account.html'
     }
 }
+document.getElementById('loginIcon').addEventListener('click', logPageSwitch)
+document.getElementById('closeLogin').addEventListener('click', logPageSwitch)
 
-const loginButton = document.getElementById('loginButton')
-loginButton.onclick = () => {
+document.getElementById('loginButton').addEventListener('click', e => {
     let email = document.getElementById('getEmail').value
     let password = document.getElementById('getPass').value
     let passConfirm = document.getElementById('getPassConfirm').value
@@ -49,11 +54,17 @@ loginButton.onclick = () => {
             if (body.isLogged) {
                 window.localStorage.setItem('email', email)
                 window.localStorage.setItem('password', password)
+                window.localStorage.setItem('isLogged', true)
+
+                document.getElementById('logPage').style.display = 'none'
+                logPageOn = false
+                window.location.href = '../pages/account.html'
             } else {
                 window.alert("senha incorreta")
+                window.localStorage.setItem('isLogged', '')
             }
         })
     } else {
         window.alert("MUDA")
     }
-}
+})
