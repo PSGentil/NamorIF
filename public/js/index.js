a497fbf7d9d2df91177c6f41f0dfe8214c088964
Pedro Samuel <108874841+PSGentil@users.noreply.github.com>
2022-11-25 22:55:56 -0300

    Transicoes, cores e legendas

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 // Index Buttons Functions
-import { enviarLogin, criarConta } from './login.js'
+import util from './util.js'
 
-let menuActive, logPageOn
+let menuActive, logPageOn, loginAccountMethod
 
 document.getElementById('menu').addEventListener('click', e => {
     let navBar = document.getElementsByTagName('nav')
@@ -31,13 +31,16 @@ function logPageSwitch(e) {
 
         if (!logPageOn) {
             logPage.style.display = 'block'
+            logPage.style.height = '15em'
+            logPage.style.transition = '.3s'
+            
             logPageOn = true
         } else {
             logPage.style.display = 'none'
             logPageOn = false
         }
-    } else {
-        window.location.href = '../pages/account.html'
+    } else if (e.target.id == 'loginIcon') {
+        window.open('../pages/account.html', '_blank')
     }
 }
 
@@ -45,25 +48,39 @@ document.getElementById('loginIcon').addEventListener('click', logPageSwitch)
 document.getElementById('closeLogin').addEventListener('click', logPageSwitch)
 
 document.getElementById('loginAccount').addEventListener('click', e => {
+    let logPage = document.getElementById('logPage')
     let title = document.getElementById('titleLogPage')
-    let confirmarSenha = document.getElementById('getPassConfirm')
+    let email = document.getElementById('getEmail')
+    let senha = document.getElementById('getPass')
     let logButton = document.getElementById('logButton')
     let switchLog = document.getElementById('switchLogCreate')
+    let switchLogClick = document.getElementById('loginAccount')
 
-    if (document.getElementById('loginAccount').method == 'login') {
+    if (loginAccountMethod == 'login') {
+        logPage.style.height = '15em'
         title.innerText = 'Criar Conta'
         switchLog.innerText = 'Já tem uma conta? '
-        confirmarSenha.style.display = 'block'
-        logButton.innerText = 'Criar Conta'
+        switchLogClick.innerText = 'Clique aqui para logar.'
+        emailCreate.style.display = 'inline'
+        email.style.display = 'none'
+        senha.style.display = 'none'
+        logButton.style.display = 'none'
 
-        document.getElementById('loginAccount').method = 'createAccount'
+        loginAccountMethod = 'createAccount'
     } else {
-        title.innerText = 'Log in LoveIF'
+        logPage.style.height = '80%'
+        title.innerText = 'Log in NamorIF'
         switchLog.innerText = 'Ainda não tem uma conta? '
-        confirmarSenha.style.display = 'none'
+        switchLogClick.innerText = 'Clique aqui para criar uma conta.'
         logButton.innerText = 'Log in'
+        logButton.style.display = "none"
+        emailCreate.style.display = "none"
+        switchLogClick.style.display = 'inline'
+        logButton.style.display = 'inline'
+        email.style.display = 'block'
+        senha.style.display = 'block'
 
-        document.getElementById('loginAccount').method = 'login'
+        loginAccountMethod = 'login'
     }
 })
 
@@ -71,38 +88,42 @@ document.getElementById('logButton').addEventListener('click', e => {
     e.preventDefault()
     let email = document.getElementById('getEmail').value
     let password = document.getElementById('getPass').value
-    let passConfirm = document.getElementById('getPassConfirm').value
 
-    if (document.getElementById('loginAccount').method == 'login') {
-        enviarLogin(email, password).then(res => {
-            if (res.status == 202) {
-                window.localStorage.setItem('email', email)
-                window.localStorage.setItem('password', password)
-                window.localStorage.setItem('isLogged', true)
-
-                document.getElementById('logPage').style.display = 'none'
-                logPageOn = false
-                window.location.href = '../pages/account.html'
-            } else {
-                window.alert("senha incorreta")
-                window.localStorage.setItem('isLogged', '')
-            }
-        })
-    } else {
-        if (password == passConfirm) {
-            criarConta(email, password).then(res => {
+    if (loginAccountMethod == 'login') {
+        if (email.includes('@')) {
+            util.enviarLogin('', email, password).then(async res => {
                 if (res.status == 202) {
-                    window.localStorage.setItem('email', email)
-                    window.localStorage.setItem('password', password)
+                    let body = await res.json()
+                    for (const key in body) {
+                        window.localStorage.setItem(key, body[key])
+                    }
                     window.localStorage.setItem('isLogged', true)
 
                     document.getElementById('logPage').style.display = 'none'
                     logPageOn = false
-                    window.location.href = '../pages/account.html'
+                    window.open('../pages/account.html', '_blank')
+                } else {
+                    window.alert("senha incorreta")
+                    window.localStorage.setItem('isLogged', '')
                 }
             })
         } else {
-            window.alert("MUDA")
+            util.enviarLogin(email, '', password).then(async res => {
+                if (res.status == 202) {
+                    let body = await res.json()
+                    for (const key in body) {
+                        window.localStorage.setItem(key, body[key])
+                    }
+                    window.localStorage.setItem('isLogged', true)
+
+                    document.getElementById('logPage').style.display = 'none'
+                    logPageOn = false
+                    window.open('../pages/account.html', '_blank')
+                } else {
+                    window.alert("senha incorreta")
+                    window.localStorage.setItem('isLogged', '')
+                }
+            })
         }
     }
 })
