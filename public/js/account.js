341bc5cdc4b1187954bafdc8d1510c945168950e
tanjirou-tk <otavioschimith124@gmail.com>
2022-11-26 22:22:34 -0300

    account 90%

--- public/js/account.js
+++ public/js/account.js
@@ -1,10 +1,12 @@
 import util from './util.js'
 
+let username = localStorage.getItem('username')
+
+if (username != null) document.getElementsByTagName('title')[0].innerText = localStorage.getItem('username')
+
 //só exemplo, muda aí
 
 const profilePhoto = await util.getImg(window.localStorage.getItem('profilePhoto'))
 
 //como colocar a image em img
 document.getElementById('profilePhotoImg').src = profilePhoto
-//como colocar a image em div
-document.getElementById('profilePhotoDiv').style.backgroundImage = `url(${profilePhoto})`
